002c9273a2e3403c10d8ca02618792c1660d55df
graphenborn <graphenborn@yandex.ru>
2022-05-01 00:59:18 +0300

    Started to do rating

---
+++ just programs/cycle.cs
@@ -0,0 +1,15 @@
+using System;
+using Math;
+
+namespace DotnetBot
+{
+   public static class Program {
+
+       public static void Main (string[] args) {
+           console.writeline("Hello world")
+       }
+
+   } 
+
+}
+
