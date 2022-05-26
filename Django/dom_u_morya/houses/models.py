7b7486d442fb0514ad0ee6d2dbf1c270f3671be1
graphenborn <graphenborn@yandex.ru>
2022-05-26 16:10:22 +0300

    created the website with Django

---
+++ Django/dom_u_morya/houses/models.py
@@ -0,0 +1,17 @@
+from pydoc import describe
+from django.db import models
+
+# Create your models here.
+class House(models.Model):
+    name = models.CharField("название", max_length=50)
+    price = models.IntegerField("цена")
+    description = models.TextField("Описание")
+    photo = models.ImageField("фотография", upload_to="houses/photos", default="", blank=True)
+
+    class Meta:
+        verbose_name = "дом"
+        verbose_name_plural = "дома"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
--- just programs/test.py
+++ just programs/test.py
@@ -1,1 +1,7 @@
-print("{0}+{1}={2}".format(5,3,5+3))
+test1 = "This is a test of the emergency text system" 
+outfile = open('file1.txt', 'wt')
+outfile.write(test1)
+outfile.close()
+with open('file1.txt', 'rt') as infile:
+    test2=infile.read()
+print(len(test2))
